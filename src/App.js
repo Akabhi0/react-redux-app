4ede35f342a094e8e2481e76b8c5f8c9c992381f
abhishek abhi <artisticdeveloperr@gmail.com>
2021-07-04 03:38:15 +0530

    css and html added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomeContainer from './container/HomeContainer'
 
 function App() {
   return (
     <div className="App">
-      react-redux
+        <HomeContainer/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="container">
+            <div className="container_card">
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-blue-select-2020?wid=940&hei=1112&fmt=png-alpha&.v=1604343704000"/>
+                <h3>I Phone</h3>
+                <h3>Rs. 62400</h3>
+                <button>Add to cart</button>
+            </div>
+            <div className="container_card">
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-blue-select-2020?wid=940&hei=1112&fmt=png-alpha&.v=1604343704000"/>
+                <h3>I Phone</h3>
+                <h3>Rs. 62400</h3>
+                <button>Add to cart</button>
+            </div>
+            <div className="container_card">
+                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-blue-select-2020?wid=940&hei=1112&fmt=png-alpha&.v=1604343704000"/>
+                <h3>I Phone</h3>
+                <h3>Rs. 62400</h3>
+                <button>Add to cart</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
